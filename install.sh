da03de28775047d4837c521d99db451223fe1f2f
Tirath Ramdas <tirath.ramdas@bromium.com>
2016-05-26 15:42:27 +1000

    Install to current $USER lib dir.

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ echo "NOT Installing custom layout"
 # cp -av U.S.ARM-MKA-3.keylayout ~/Library/Keyboard\ Layouts/
 
 echo "Installing Karabiner remapping configuration."
-cp -av private-pure.xml /Users/dwi/Library/Application\ Support/Karabiner/private.xml
+cp -av private-pure.xml /Users/"$USER"/Library/Application\ Support/Karabiner/private.xml
 
 echo "Installation done, open Karabiner then reload XML and enable configuration."
